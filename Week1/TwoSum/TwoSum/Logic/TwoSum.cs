83fbe4fc3be23694383629a8d72270b51faccf62
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2024-12-05 18:35:37 +0200

    add hash table algo

--- Week1/TwoSum/TwoSum/Logic/TwoSum.cs
+++ Week1/TwoSum/TwoSum/Logic/TwoSum.cs
@@ -2,12 +2,33 @@
 {
 	public class TwoSum
 	{
-		public int[] Algo1(int[] nums, int target)
+		//time complexity O(n^2), space complexity O(1)
+		public int[] BruteForceAlgo(int[] nums, int target)
 		{
 			for (int i = 0; i < nums.Length; i++)
 				for (int j = i + 1; j < nums.Length; j++)
 					if (nums[i] + nums[j] == target)
 						return new int[] { i, j };
+
+			throw new Exception("No such indexes");
+		}
+
+		//time complexity O(n), space complexity O(n)
+		public int[] HashAlgo(int[] nums, int target)
+		{
+			//First pass - fill dictionary
+			//Dictionary: value, index
+			Dictionary<int, int> hash = new Dictionary<int, int>();
+			for (int i = 0; i < nums.Length; i++)
+				hash[nums[i]] = i;
+
+			//Second pass search num
+			for (int i = 0; i < nums.Length; i++)
+			{
+				int numToNeed = target - nums[i];
+				if (hash.ContainsKey(numToNeed) && hash[numToNeed] != i)
+					return new int[] { i, hash[numToNeed] };
+			}
 
 			throw new Exception("No such indexes");
 		}
--- Week1/TwoSum/TwoSum/TestProject1/Test1.cs
+++ Week1/TwoSum/TwoSum/TestProject1/Test1.cs
@@ -6,27 +6,53 @@ namespace TestProject1
 	public sealed class Test1
 	{
 		[TestMethod]
-		public void TestMethod1()
+		public void TestBruteForce1()
 		{
 			TwoSum twoSum = new TwoSum();
 
 			int[] nums = [2, 7, 11, 15];
 			int[] expected = [0, 1];
 			int target = 9;
-			int[] actual = twoSum.Algo1(nums, target);
+			int[] actual = twoSum.BruteForceAlgo(nums, target);
 
 			CollectionAssert.AreEqual(expected, actual);
 		}
 
 		[TestMethod]
-		public void TestMethod2()
+		public void TestBruteForce2()
 		{
 			TwoSum twoSum = new TwoSum();
 
 			int[] nums = [3, 2, 4];
 			int[] expected = [1, 2];
 			int target = 6;
-			int[] actual = twoSum.Algo1(nums, target);
+			int[] actual = twoSum.BruteForceAlgo(nums, target);
+
+			CollectionAssert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void TestHash1()
+		{
+			TwoSum twoSum = new TwoSum();
+
+			int[] nums = [2, 7, 11, 15];
+			int[] expected = [0, 1];
+			int target = 9;
+			int[] actual = twoSum.HashAlgo(nums, target);
+
+			CollectionAssert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void TestHash2()
+		{
+			TwoSum twoSum = new TwoSum();
+
+			int[] nums = [3, 2, 4];
+			int[] expected = [1, 2];
+			int target = 6;
+			int[] actual = twoSum.HashAlgo(nums, target);
 
 			CollectionAssert.AreEqual(expected, actual);
 		}
