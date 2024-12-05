b7346d5754010283a288b00de0f3d24834172fdb
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2024-12-05 18:03:57 +0200

    rfct

--- Week1/TwoSum/TwoSum/Logic/TwoSum.cs
+++ Week1/TwoSum/TwoSum/Logic/TwoSum.cs
@@ -4,17 +4,10 @@
 	{
 		public int[] Algo1(int[] nums, int target)
 		{
-			int[] indexes = new int[2];
-
 			for (int i = 0; i < nums.Length; i++)
 				for (int j = i + 1; j < nums.Length; j++)
 					if (nums[i] + nums[j] == target)
-					{
-						indexes[0] = i;
-						indexes[1] = j;
-						return indexes;
-					}
-
+						return new int[] { i, j };
 			return null;
 		}
 	}
