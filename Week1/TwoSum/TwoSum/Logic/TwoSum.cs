201692f3cd291e97d78836247f05fb10790afef3
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2024-12-05 17:54:58 +0200

    first impl

---
+++ Week1/TwoSum/TwoSum/Logic/TwoSum.cs
@@ -0,0 +1,21 @@
+﻿namespace Logic
+{
+	public class TwoSum
+	{
+		public int[] Algo1(int[] nums, int target)
+		{
+			int[] indexes = new int[2];
+
+			for (int i = 0; i < nums.Length; i++)
+				for (int j = i + 1; i < nums.Length; i++)
+					if (nums[i] + nums[j] == target)
+					{
+						indexes[0] = i;
+						indexes[1] = j;
+						return indexes;
+					}
+
+			return null;
+		}
+	}
+}
---
+++ Week1/TwoSum/TwoSum/TestProject1/Test1.cs
@@ -0,0 +1,34 @@
+﻿using Logic;
+
+namespace TestProject1
+{
+	[TestClass]
+	public sealed class Test1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			TwoSum twoSum = new TwoSum();
+
+			int[] nums = [2, 7, 11, 15];
+			int[] expected = [0, 1];
+			int target = 9;
+			int[] actual = twoSum.Algo1(nums, target);
+
+			CollectionAssert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void TestMethod2()
+		{
+			TwoSum twoSum = new TwoSum();
+
+			int[] nums = [3, 2, 4];
+			int[] expected = [2, 1];
+			int target = 6;
+			int[] actual = twoSum.Algo1(nums, target);
+
+			CollectionAssert.AreEqual(expected, actual);
+		}
+	}
+}
