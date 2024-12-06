ce226d3f39c87a73c7524c5391b2c38c64531bcd
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2024-12-06 07:07:33 +0200

    brute force

---
+++ Week1/Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock/StocksBuy.cs
@@ -0,0 +1,17 @@
+﻿namespace Best_Time_to_Buy_and_Sell_Stock
+{
+	public class StocksBuy
+	{
+		public int BruteForceAlgo(int[] prices)
+		{
+			int maxRevenue = 0;
+
+			for (int i = 0; i < prices.Length; i++)
+				for (int j = i + 1; j < prices.Length; j++)
+					if (prices[j] - prices[i] > maxRevenue)
+						maxRevenue = prices[j] - prices[i];
+
+			return maxRevenue;
+		}
+	}
+}
---
+++ Week1/Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock/TestProject1/Test1.cs
@@ -0,0 +1,32 @@
+﻿using Best_Time_to_Buy_and_Sell_Stock;
+
+namespace TestProject1
+{
+	[TestClass]
+	public sealed class Test1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			StocksBuy stocksBuy = new StocksBuy();
+
+			int[] prices = [7, 1, 5, 3, 6, 4];
+			int expected = 5;
+			int actual = stocksBuy.BruteForceAlgo(prices);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void TestMethod2()
+		{
+			StocksBuy stocksBuy = new StocksBuy();
+
+			int[] prices = [7,6,4,3,1];
+			int expected = 0;
+			int actual = stocksBuy.BruteForceAlgo(prices);
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
