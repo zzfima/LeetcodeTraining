e0ef90eb1380845b9c3d6c14a61125c581493aa1
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2024-12-19 11:09:36 +0200

    done!

--- Week2/InvertBinaryTree/InvertBinaryTree/InvertBinaryTree/Program.cs
+++ Week2/InvertBinaryTree/InvertBinaryTree/InvertBinaryTree/Program.cs
@@ -10,9 +10,31 @@ namespace InvertBinaryTree
              * Output: [1,3,2,7,6,5,4]
              */
 			TreeNode root = CreateTreeFromArray([1, 2, 3, 4, 5, 6, 7], 0);
-			TreeNode reversedTree = ReverseTree(root);
+			TreeNode reversedTree = InvertTree(root);
 
 			Console.ReadLine();
+		}
+
+		static TreeNode InvertTree(TreeNode root)
+		{
+			if (root == null)
+				return null;
+
+			//invert children
+			TreeNode tmp = root.left;
+			root.left = root.right;
+			root.right = tmp;
+
+			//do recursive operation on left child
+			if (root.left != null)
+				root.left = InvertTree(root.left);
+
+
+			//do recursive operation on right child
+			if (root.right != null)
+				root.right = InvertTree(root.right);
+
+			return root;
 		}
 
 		private static TreeNode CreateTreeFromArray(int[] ints, int i)
@@ -28,22 +50,17 @@ namespace InvertBinaryTree
 			if (i * 2 + 1 >= ints.Length)
 				return root;
 
-			root.Left = CreateTreeFromArray(ints, i * 2 + 1);
-			root.Right = CreateTreeFromArray(ints, i * 2 + 2);
+			root.left = CreateTreeFromArray(ints, i * 2 + 1);
+			root.right = CreateTreeFromArray(ints, i * 2 + 2);
 
 			return root;
-		}
-
-		private static TreeNode ReverseTree(TreeNode root)
-		{
-			throw new NotImplementedException();
 		}
 	}
 
 	public class TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
 	{
-		public int Val = val;
-		public TreeNode Left = left;
-		public TreeNode Right = right;
+		public int val = val;
+		public TreeNode left = left;
+		public TreeNode right = right;
 	}
 }
