9d3e4920dffa71c3050dc7ab26e67ff69c1c607a
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2024-12-31 09:17:53 +0200

    start recursion journey

---
+++ Week2/Recursion/MaximumBinaryTreeDepth/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MaximumBinaryTreeDepth
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			TreeNode node1 = new TreeNode(1);
+			TreeNode node2 = new TreeNode(2);
+			TreeNode node3 = new TreeNode(3);
+			TreeNode node4 = new TreeNode(4);
+
+			node1.left = node2;
+			node1.right = node3;
+			node3.left = node4;
+
+			int maxDepth = MaxDepth(node1);
+
+			Console.ReadLine();
+		}
+
+		static int MaxDepth(TreeNode root)
+		{
+			if (root == null) return 0;
+
+			int leftDepth = MaxDepth(root.left);
+			int rightDepth = MaxDepth(root.right);
+
+			return Math.Max(leftDepth, rightDepth) + 1;
+		}
+	}
+}
