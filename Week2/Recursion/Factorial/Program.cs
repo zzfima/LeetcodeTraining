edb4369ca93d321338191a4bf89416c90e5c7cac
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2024-12-31 13:16:43 +0200

    impl factorial

---
+++ Week2/Recursion/Factorial/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Factorial
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int fct = Factorial(5);
+		}
+
+		public static int Factorial(int i)
+		{
+			if (i == 0) return 1;
+			else return i * Factorial(i - 1);
+		}
+	}
+}
