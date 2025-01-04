84ea4d1824b1482928d24d60b977a836714dc39f
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2025-01-04 13:15:48 +0200

    recursion find max in array

---
+++ Week2/Recursion/FindMaxNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FindMaxNumber
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int max = FindMax([3, 7, 1, 9, 3, 8, 11, 2, 100]);
+		}
+
+		static int FindMax(int[] nums)
+		{
+			if (nums.Length == 1) return nums[0];
+			if (nums.Length == 2) return Math.Max(nums[0], nums[1]);
+
+			int middle = nums.Length / 2;
+
+			return Math.Max(FindMax(nums[..middle]), FindMax(nums[middle..]));
+		}
+	}
+}
