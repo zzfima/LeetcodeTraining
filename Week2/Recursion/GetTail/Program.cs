43961d9e5d9384020600a682df39dc525bbcd02d
Efim Zabarsky <[Efim.Zabarsky@kla-tencor.com]>
2025-01-05 09:50:49 +0200

    Get array Tail

---
+++ Week2/Recursion/GetTail/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GetTail
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int l = GetArrayTail([4, 5, 6, 7, 8]);
+		}
+
+		static int GetArrayTail(int[] nums)
+		{
+			if (nums.Length == 1) return nums[0];
+
+			return GetArrayTail(nums[1..]);
+		}
+	}
+}
